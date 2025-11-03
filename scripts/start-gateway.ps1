eee4bf981aa4c7e8847ed8836e2ec3ee1b2c3124
sam parlatore <134798998+ParlAquatics@users.noreply.github.com>
2025-11-03 15:15:17 -0600

    another retry...
    getting PowerShell window to stay open

--- scripts/start-gateway.ps1
+++ scripts/start-gateway.ps1
@@ -1,2 +1,4 @@
 Write-Host "Starting gateway..."
-Start-Process "java" "-cp ../ParlaGateway/target/ParlaMapper-1.0-SNAPSHOT.jar:lib/* com.parlAquatics.gateway.ParlaGateway"
+Start-Process "java" `
+  -ArgumentList "-cp ../ParlaGateway/target/ParlaMapper-1.0-SNAPSHOT.jar:lib/* com.parlAquatics.gateway.ParlaGateway"`
+  -NoNewWindow -Wait
--- scripts/start-test-server.ps1
+++ scripts/start-test-server.ps1
@@ -1,2 +1,4 @@
 Write-Host "Starting test server..."
-Start-Process "java" "-cp ../ParlaGateway/target/ParlaMapper-1.0-SNAPSHOT.jar:lib/* com.parlAquatics.gateway.simulation.quickFIXServer.QuickFIXServer"
+Start-Process "java" `
+  -ArgumentList "-cp ../ParlaGateway/target/ParlaMapper-1.0-SNAPSHOT.jar;lib/* com.parlAquatics.gateway.simulation.quickFIXServer.QuickFIXServer" `
+  -NoNewWindow -Wait
