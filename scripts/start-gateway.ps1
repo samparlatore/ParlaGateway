0df6cf91b5494d443dab308430b58bf24255bc0d
sam parlatore <134798998+ParlAquatics@users.noreply.github.com>
2025-11-03 15:20:25 -0600

    haha
    jar name

--- scripts/start-gateway.ps1
+++ scripts/start-gateway.ps1
@@ -1,4 +1,4 @@
 Write-Host "Starting gateway..."
 Start-Process "java" `
-  -ArgumentList "-cp ../ParlaGateway/target/ParlaMapper-1.0-SNAPSHOT.jar:lib/* com.parlAquatics.gateway.ParlaGateway"`
+  -ArgumentList "-cp ../ParlaGateway/target/ParlaGateway-1.0-SNAPSHOT.jar:lib/* com.parlAquatics.gateway.ParlaGateway"`
   -NoNewWindow -Wait
