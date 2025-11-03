6e8e02a7a93fbdd65023520f8992cdbf7ac37d28
sam parlatore <134798998+ParlAquatics@users.noreply.github.com>
2025-11-03 15:21:11 -0600

    lol
    git abuse!

--- scripts/start-test-server.ps1
+++ scripts/start-test-server.ps1
@@ -1,4 +1,4 @@
 Write-Host "Starting test server..."
 Start-Process "java" `
-  -ArgumentList "-cp ../ParlaGateway/target/ParlaGateway-1.0-SNAPSHOT.jar;lib/* com.parlAquatics.gateway.simulation.quickFIXServer.QuickFIXServer" `
+  -ArgumentList "-cp ./ParlaGateway/target/ParlaGateway-1.0-SNAPSHOT.jar;lib/* com.parlAquatics.gateway.simulation.quickFIXServer.QuickFIXServer" `
   -NoNewWindow -Wait
